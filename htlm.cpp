e46b9d0a792dd1c55552e8bf537ac9977e73651f
scarret <luislopez188@hotmail.com>
2017-08-08 23:00:39 -0600

    Update htlm.cpp

--- htlm.cpp
+++ htlm.cpp
@@ -5,12 +5,25 @@ Descripcion:	Programa para verificar la sintaxis simple de un documento HTML
 Version:	Beta 0.4   Compilado en (g++ version 5.4.0 20160609 (Debian 5.4.0-6))
 */
 
+
+/*
+----------TABLA DE VERDAD DE ERRORES-------------------------
+
+	INICIO 	FIN
+	____________
+	|  0  |  0  |-- No se declaro la etiqueta  se utiliza el  else
+	|  0  |  1  |-- Se finalizo la etiqueta, pero nunca se aperturo
+	|  1  |  0  |-- No se finalizo la etiqueta
+	|__1__|__1__|--	No Problem
+
+*/
+
 #include<iostream>
 #include<fstream>
 
 using namespace std;
 int n=0;
-bool head_start =true, head_end =true, html_s =true, html_e =true, my_h =true, body_s =true, title_s=true, body_e=true, title_e=true;
+bool head_start =true, head_end =true, html_s =true, html_e =true, my_h =true, body_s =true, title_s=true, body_e=true, title_e=true, error=true;
 
 int main()
 {
@@ -82,7 +95,7 @@ cout<<""<<endl;
 							fichero.get(letra);
 							if(letra == '>')
 							head_start=false;
-							cout<<"heee inicio  el head"<<endl;
+
 						}
 					}
 					break;
@@ -98,7 +111,7 @@ cout<<""<<endl;
 						{
                                                        fichero.get(letra);
                                                        if(letra == '>')
-                                                       cout<<"heee inicia  el html"<<endl;
+
 							html_s=false;
 						}
 					}
@@ -122,7 +135,7 @@ cout<<""<<endl;
 					{
 						fichero.get(letra);
 	                                        if(letra =='>')
-						cout<<"hee se inicio el body"<<endl;
+
 						body_s=false;
 					}
 				}
@@ -145,7 +158,7 @@ cout<<""<<endl;
         	                                {
                 	                                fichero.get(letra);
                         	                        if(letra =='>')
-                                	               { cout<<"hee se inicio el title"<<endl;
+                                	               { 
                                         	        title_s=false;}
   	                                     	 }
 					}
@@ -180,7 +193,7 @@ cout<<""<<endl;
 							fichero.get(letra);
 							if(letra == '>')
 							head_end=false;
-							cout<<"heee finaliza  el head"<<endl;
+
 						}
 					}
 					break;
@@ -196,7 +209,7 @@ cout<<""<<endl;
 						{
                                                        fichero.get(letra);
                                                        if(letra == '>')
-                                                       cout<<"heee finaliza  el html"<<endl;
+
 							html_e=false;
 						}
 					}
@@ -220,7 +233,7 @@ cout<<""<<endl;
 					{
 						fichero.get(letra);
 	                                        if(letra =='>')
-						cout<<"hee se finaliz el body"<<endl;
+						
 						body_e=false;
 					}
 				}
@@ -243,7 +256,7 @@ cout<<""<<endl;
         	                                {
                 	                                fichero.get(letra);
                         	                        if(letra =='>')
-                                	               { cout<<"hee se finaliza el title"<<endl;
+                                	               { 
                                         	        title_e=false;}
   	                                     	 }
 					}
@@ -277,5 +290,73 @@ cout<<""<<endl;
         else {cout <<"error";}
 
 
+	 if (head_start==true && head_end==true)
+	{
+		cout<<"[!] Error no se declaro la etiqueta <head>"<<endl;
+	}
+	else if (head_start==false && head_end==true)
+	{
+		cout<<"[!] Error No se finalizo la etiqueta <head>, utiliza </etiqueta> para cerrar las etiquetas"<<endl;
+	}
+	else if (head_start==true && head_end==false)
+	{
+		cout<<"[!] Error Se finalizo la etiqueta <head>, pero nunca se declaro"<<endl;
+	}
+
+
+
+	 if (html_s==true && html_e==true)
+	{
+		cout<<"[!] Error no se declaro la etiqueta <html>"<<endl;
+	}
+	else if (html_s==false && html_e==true)
+	{
+		cout<<"[!] Error No se finalizo la etiqueta <html>, utiliza </etiqueta> para cerrar las etiquetas"<<endl;
+	}
+	else if (html_s==true && html_e==false)
+	{
+		cout<<"[!] Error Se finalizo la etiqueta <html>, pero nunca se inicio"<<endl;
+	}
+	else 
+	{error=false;
+	}
+
+
+	 if (body_s==true && body_e==true)
+	{
+		cout<<"[!] Error no se declaro la etiqueta <body>"<<endl;
+	}
+	else if (body_s==false && body_e==true)
+	{
+		cout<<"[!] Error No se finalizo la etiqueta <body>, utiliza </etiqueta> para cerrar las etiquetas"<<endl;
+	}
+	else if (body_s==true && body_e==false)
+	{
+		cout<<"[!] Error Se finalizo la etiqueta <body>, pero nunca se inicio"<<endl;
+	}
+	else 
+	{error=false;
+	}
+
+	 if (title_s==true && title_e==true)
+	{
+		cout<<"[!] Error no se declaro la etiqueta <title>"<<endl;
+	}
+	else if (title_s==false && title_e==true)
+	{
+		cout<<"[!] Error No se finalizo la etiqueta <title>, utiliza </etiqueta> para cerrar las etiquetas"<<endl;
+	}
+	else if (title_s==true && title_e==false)
+	{
+		cout<<"[!] Error Se finalizo la etiqueta <title>, pero nunca se inicio"<<endl;
+	}
+	else 
+	{error=false;
+	}
+
+	if (error == false)
+	{
+		cout<<"\n [+] No se detectaron errores de 'sintasix' !!! \n\n"<<endl;
+	} 
 return 0;
 }
